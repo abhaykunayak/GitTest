1e048fa7b2990277cb20d0d876f74a58979c452c
abhaykunayak <abhaykunayak@gmail.com>
2015-12-01 17:06:53 +0200

    Added second plot (p2) y = sin(x)^2

--- GitTest.m
+++ GitTest.m
@@ -1,10 +1,16 @@
 x = 0:0.01:4*pi;
 y = sin(x);
 
-figure;
-p = plot(x, y);
-p.LineStyle = '-';
-p.LineWidth = 2;
+fig = figure;
+hold on;
+
+p1 = plot(x, y);
+p1.LineStyle = '-';
+p1.LineWidth = 2;
+
+p2 = plot(x, y.^2);
+p2.LineStyle = '-';
+p2.LineWidth = 2;
 
 xlabel('x');
 ylabel('y');
